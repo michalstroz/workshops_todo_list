3981fcda398068b214ca32d55c818ff4a217ddde
Michał <michalstroz@gmail.com>
2018-05-27 13:19:16 +0200

    add date/time validation in model by creating new validate method

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,9 @@
 class Task < ApplicationRecord
   validates :title, presence: true
+  validate :deadline_cannot_be_in_the_past
+
+  def deadline_cannot_be_in_the_past
+    errors.add(:deadline, "can't be in the past") if
+      !deadline.blank? and deadline < Date.today
+  end
 end
