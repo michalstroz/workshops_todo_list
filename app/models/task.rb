45048117459444f2994de38ccad77dfa3df5a646
Michał <michalstroz@gmail.com>
2018-05-27 13:22:33 +0200

    improve task, deadline validation. Deadline is matched to Time class now.

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,6 @@ class Task < ApplicationRecord
 
   def deadline_cannot_be_in_the_past
     errors.add(:deadline, "can't be in the past") if
-      !deadline.blank? and deadline < Date.today
+      !deadline.blank? and deadline <= Time.now
   end
 end
